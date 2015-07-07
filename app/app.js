3a3587f2dd5d2a6e022c1abbc65f8f86175bf099
longto <long.to@niteco.se>
2015-07-07 12:40:58 +0700

    create isolated scope for directive

--- app/app.js
+++ app/app.js
@@ -8,7 +8,8 @@ angular.module('myApp', [
     'myApp.welcome',
     'myApp.addPost',
     'albumControllers',
-    'albumServices'
+    'albumServices',
+    'directivesFrontEnd'
 ])
 .config(['$routeProvider',
     function($routeProvider) {
---
+++ app/directives/directives.js
@@ -0,0 +1,19 @@
+angular.module('directivesFrontEnd',[])
+ .directive('news', function () {
+      
+      var controller = function () {
+              var vm = this;
+              function init() {                
+                //vm.articles = angular.copy(vm.dataStore);               
+              }
+              init();
+      };    
+      return {
+          restrict: 'EA', //Default for 1.3+
+          scope : { data : '=' },
+          controller: controller,
+          controllerAs: 'vm',
+          bindToController: true, //required in 1.3+ with controllerAs
+          templateUrl: 'directives/newsTemplate.html'
+      };
+  });
